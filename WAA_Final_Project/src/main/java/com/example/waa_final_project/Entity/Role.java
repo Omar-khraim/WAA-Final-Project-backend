39c8512c7908ed60afa7debff584bdf6aebfa834
aabdelaziz <aboukham.abdelaziz@gmail.com>
2023-02-06 15:26:45 -0600

    first task

---
+++ WAA_Final_Project/src/main/java/com/example/waa_final_project/Entity/Role.java
@@ -0,0 +1,5 @@
+package com.example.waa_final_project.Entity;
+
+public enum Role {
+    ADMIN, CUSTOMER, OWNER
+}
--- WAA_Final_Project/src/main/java/com/example/waa_final_project/Entity/Users.java
+++ WAA_Final_Project/src/main/java/com/example/waa_final_project/Entity/Users.java
@@ -1,4 +1,23 @@
 package com.example.waa_final_project.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Users {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private Role role;
 }
