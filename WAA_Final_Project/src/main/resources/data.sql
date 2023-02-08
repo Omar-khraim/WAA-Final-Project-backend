eeb518ae54e4560b5cb6ca71470df8eba38a904b
Mkiwanz <117238114+Mkiwanz@users.noreply.github.com>
2023-02-08 17:03:17 -0600

    Update data.sql

--- WAA_Final_Project/src/main/resources/data.sql
+++ WAA_Final_Project/src/main/resources/data.sql
@@ -14,13 +14,19 @@ values ('mkiwan@yahoo.com' , 'Moh Kiwan' , '$2a$12$IKEQb00u5QpZMx4v5zMweu.3wrq0p
 
 insert into public.address ( city, state, street, zip_code)
 values ('Fairfield', 'Iowa', '4th street', 52557);
+insert into public.address ( city, state, street, zip_code)
+values ('IowaCity', 'Iowa', '6th street', 52240);
+insert into public.address ( city, state, street, zip_code)
+values ('Des Moines', 'Iowa', '8th street', 50047);
 
 
 
 insert into property ( area_in_square_feet, bathroom_num, room_num, status,price, address_id, owner_id)
 values (950, 2, 3, 1,35000, 1, 2);
 insert into property ( area_in_square_feet, bathroom_num, room_num, status, price, address_id, owner_id)
-values (950, 2, 3, 1,100000, 1, 2);
+values (950, 2, 3, 1,100000, 3, 2);
+insert into property ( area_in_square_feet, bathroom_num, room_num, status, price, address_id, owner_id)
+values (1200, 3, 2, 2,150000, 2, 2);
 
 
 insert into offer (offer_description, status, agent_email, customer_id, property_id)
