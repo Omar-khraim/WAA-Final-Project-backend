95bc13b6af34d42d950ae6f861fc2b6a2f26881e
Mkiwanz <117238114+Mkiwanz@users.noreply.github.com>
2023-02-09 14:41:09 -0600

    Update data.sql

--- WAA_Final_Project/src/main/resources/data.sql
+++ WAA_Final_Project/src/main/resources/data.sql
@@ -30,7 +30,7 @@ values (950, 2, 3, 1, 35000, 1, 1);
 insert into property (area_in_square_feet, bathroom_num, room_num, status, price, address_id, owner_id)
 values (950, 2, 3, 1, 100000, 3, 1);
 insert into property (area_in_square_feet, bathroom_num, room_num, status, price, address_id, owner_id)
-values (1200, 3, 2, 2, 150000, 2, 1);
+values (1200, 3, 2, 1, 150000, 2, 1);
 
 
 -- insert into offer (offer_description, status, agent_email, customer_id, property_id)
@@ -40,12 +40,16 @@ values (1200, 3, 2, 2, 150000, 2, 1);
 -- values ('', 500, 5000, 'i want your house', 'house', 1, 2, 2);
 -- values ('', 600, 4900, 'i want your house too ', 'house', 1, 2, 1);
 
-INSERT INTO public.offer ( agent_email, credit_score, offer_amount, offer_description, offer_type, status, property_id, user_id)
-VALUES ( null, 500, 500, 'mkiwan offer', '', 0, 1, 2);
-INSERT INTO public.offer ( agent_email, credit_score, offer_amount, offer_description, offer_type, status, property_id, user_id)
-VALUES ( null, 600, 600, 'mkiwan offer', '', 0, 2, 2);
-INSERT INTO public.offer ( agent_email, credit_score, offer_amount, offer_description, offer_type, status, property_id, user_id)
-VALUES ( null, 660, 123456, 'kiki offer', '', 0, 1, 3);
+
+INSERT INTO public.offer (id, agent_email, credit_score, offer_amount, offer_description, offer_type, status,
+                          property_id, user_id)
+VALUES (1, null, 500, 500, 'mkiwan offer', '', 4, 1, 2);
+INSERT INTO public.offer (id, agent_email, credit_score, offer_amount, offer_description, offer_type, status,
+                          property_id, user_id)
+VALUES (2, null, 600, 600, 'mkiwan offer', '', 4, 2, 2);
+INSERT INTO public.offer (id, agent_email, credit_score, offer_amount, offer_description, offer_type, status,
+                          property_id, user_id)
+VALUES (3, null, 660, 123456, 'kiki offer', '', 4, 1, 3);
 
 
 insert into likes (property_id, users_id)
